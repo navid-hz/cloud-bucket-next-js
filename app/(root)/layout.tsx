f7a5afe44be39a7a8a40261754627c4065165cec
Navid Hz <hosseini.zari@gmail.com>
2024-12-21 23:42:19 +0100

    Mobile Navigation added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import Sidebar from '@/components/Sidebar'
-import MobileNavigation from '@/components/MobileNavigation'
-import Header from '@/components/Header'
+import React from "react";
+import Sidebar from "@/components/Sidebar";
+import MobileNavigation from "@/components/MobileNavigation";
+import Header from "@/components/Header";
+import { getCurrentUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+//import { Toaster } from "@/components/ui/toaster";
 
-function layout({ children } : { children: React.ReactNode }) {
+export const dynamic = "force-dynamic";
+
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return redirect("/sign-in");
+
   return (
     <main className="flex h-screen">
-        <Sidebar/>
-        <section className="flex h-full flex-1 flex-col">
-            <MobileNavigation/>
-            <Header/>
-            <div className='main-content'>{children}</div>
-        </section>
+      <Sidebar {...currentUser} />
+
+      <section className="flex h-full flex-1 flex-col">
+        <MobileNavigation {...currentUser} />
+        <Header userId={currentUser.$id} accountId={currentUser.accountId} />
+        <div className="main-content">{children}</div>
+      </section>
+
+      {/* <Toaster /> */}
     </main>
   );
-}
-
-export default layout;
+};
+export default Layout;
