9d1bab40f15dc7ea27fd88c2754cb66015b65026
Navid Hz <hosseini.zari@gmail.com>
2024-12-22 02:17:03 +0100

    Upload file functionality added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,41 @@
-import React from 'react';
-import {Button} from '@/components/ui/button';
-import Image from 'next/image';
-import Search from '@/components/Search';
-import FileUploader from '@/components/FileUploader';
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Search from "@/components/Search";
+import FileUploader from "@/components/FileUploader";
+import { signOutUser } from "@/lib/actions/user.actions";
 
-function Header() {
+const Header = ({
+  userId,
+  accountId,
+}: {
+  userId: string;
+  accountId: string;
+}) => {
   return (
-    <header>
-        <Search />
-        <div>
-            <FileUploader />
-            <form action="">
-                <Button type='submit' className='sign-out-button' >
-                    <Image 
-                        src='/assets/icons/logout.svg'
-                        alt='logout'
-                        width={24}
-                        height={24}
-                        className='w-6'
-                    />
-                </Button>
-            </form>
-        </div>
-    </header>
-  )
-}
+    <header className="header">
+      <Search />
+      <div className="header-wrapper">
+        <FileUploader ownerId={userId} accountId={accountId} />
+        <form
+          action={async () => {
+            "use server";
 
-export default Header
+            await signOutUser();
+          }}
+        >
+          <Button type="submit" className="sign-out-button">
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="logo"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+        </form>
+      </div>
+    </header>
+  );
+};
+export default Header;
