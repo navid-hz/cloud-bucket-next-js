441bb893435105e6c4729275c7ef07c7178b4d8f
Navid Hz <hosseini.zari@gmail.com>
2025-01-05 16:23:56 +0100

    layout fixed

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,30 +1,50 @@
 import React from "react";
-import Sidebar from "@/components/Sidebar";
-import MobileNavigation from "@/components/MobileNavigation";
-import Header from "@/components/Header";
-import { getCurrentUser } from "@/lib/actions/user.actions";
-import { redirect } from "next/navigation";
-import { Toaster } from "@/components/ui/toaster";
+import Image from "next/image";
 
-export const dynamic = "force-dynamic";
-
-const Layout = async ({ children }: { children: React.ReactNode }) => {
-  const currentUser = await getCurrentUser();
-
-  if (!currentUser) return redirect("/sign-in");
-
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex h-screen">
-      <Sidebar {...currentUser} />
+    <div className="flex min-h-screen">
+      <section className="hidden w-1/2 items-center justify-center bg-brand p-10 lg:flex xl:w-2/5">
+        <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="logo"
+            width={224}
+            height={82}
+            className="h-auto"
+          />
 
-      <section className="flex h-full flex-1 flex-col">
-        <MobileNavigation {...currentUser} />
-        <Header userId={currentUser.$id} accountId={currentUser.accountId} />
-        <div className="main-content">{children}</div>
+          <div className="space-y-5 text-white">
+            <h1 className="h1">Manage your files the best way</h1>
+            <p className="body-1">
+              This is a place where you can store all your documents.
+            </p>
+          </div>
+          <Image
+            src="/assets/images/files.png"
+            alt="Files"
+            width={342}
+            height={342}
+            className="transition-all hover:rotate-2 hover:scale-105"
+          />
+        </div>
       </section>
 
-      <Toaster />
-    </main>
+      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+        <div className="mb-16 lg:hidden">
+          <Image
+            src="/assets/icons/logo-full-brand.svg"
+            alt="logo"
+            width={224}
+            height={82}
+            className="h-auto w-[200px] lg:w-[250px]"
+          />
+        </div>
+
+        {children}
+      </section>
+    </div>
   );
 };
+
 export default Layout;
--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -5,24 +5,32 @@ import { appwriteConfig } from "@/lib/appwrite/config";
 import { cookies } from "next/headers";
 
 export const createSessionClient = async () => {
+
+  const cookieStore = await cookies();
+  const session = cookieStore.get('appwrite-session');
+
+  console.log("session", session);
+
+  if (!session || !session.value) {
+    return null;
+  }
+
   const client = new Client()
     .setEndpoint(appwriteConfig.endpointUrl)
     .setProject(appwriteConfig.projectId);
 
-  const session = (await cookies()).get("appwrite-session");
+  const account = new Account(client);
+  const databases = new Databases(client);
 
-  if (!session || !session.value) throw new Error("No session");
+  
+
+  
+
+  //if (!session || !session.value) throw new Error("No session");
 
   client.setSession(session.value);
 
-  return {
-    get account() {
-      return new Account(client);
-    },
-    get databases() {
-      return new Databases(client);
-    },
-  };
+  return { client, account, databases };
 };
 
 export const createAdminClient = async () => {
